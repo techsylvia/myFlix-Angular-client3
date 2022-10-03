20921705aed271a3d3bf95adbdd2189a1819d543
Sylvia <sylvia@donchev.is>
2022-10-03 12:38:22 +0200

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,14 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
-
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
@@ -39,7 +45,9 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    RouterModule.forRoot(appRoutes),
+    MatIconModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
